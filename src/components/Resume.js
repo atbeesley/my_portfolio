2796373b9a5372c9c3e97d85901bced824c20bcf
Alex Beesley <alexander.beesley@gmail.com>
2020-01-30 11:26:34 +0000

    adds BrowserRouter to index.js

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 
-class LandingPage extends Component {
+class Resume extends Component {
   render(
     return(
       <div>
-      <h1>Landing Page </h1>
+      <h1>Resume Page </h1>
       </div>
     )
   )
 }
 
-export default LandingPage;
+export default Resume;
