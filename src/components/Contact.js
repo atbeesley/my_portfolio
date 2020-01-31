ab3b8ef02f17bea6415e2756767b3ecff87f6020
Alex Beesley <alexander.beesley@gmail.com>
2020-01-31 11:56:59 +0000

    adds contact columns and list

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,38 @@
 import React, { Component } from 'react';
+import { Grid, Cell, List, ListItem, ListItemContent } from 'react-mdl';
 
 class Contact extends Component {
   render(){
     return(
-      <div>
-      <h1>Contact </h1>
+      <div className="contact-body">
+        <Grid className="contact-grid">
+          <Cell col={6}>
+            <h2>Alex Beesley</h2>
+            <img src="https://i.imgur.com/hNsZK7z.png"
+              alt="avatar"
+              className="avatar-image"
+              />
+              <p style={{ width: '75%', margin: 'auto', paddingTop: '1em' }}>this is some text</p>
+          </Cell>
+          <Cell col={6}>
+            <h2>Contact me</h2>
+            <hr />
+            <div className="contact-list">
+            <List>
+      <ListItem>
+        <ListItemContent icon="person">Bryan Cranston</ListItemContent>
+      </ListItem>
+      <ListItem>
+        <ListItemContent icon="person">Aaron Paul</ListItemContent>
+      </ListItem>
+      <ListItem>
+        <ListItemContent icon="person">Bob Odenkirk</ListItemContent>
+      </ListItem>
+    </List>
+</div>
+          </Cell>
+
+        </Grid>
       </div>
     )
   }
