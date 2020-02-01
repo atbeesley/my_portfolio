ed572295fd6bb2856dc7acf8babd88a407d4b2d4
Alex Beesley <alexander.beesley@gmail.com>
2020-02-01 13:39:38 +0000

    formats cv page with avatar and bootstrap

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react';
+import { Grid, Cell } from 'react-mdl';
 
 class Resume extends Component {
   render(){
     return(
       <div>
-      <h1>Resume Page </h1>
+        <Grid>
+          <Cell className="resume-left-col"col={4}>
+            <div style={{textAlign: 'center'}}>
+              <img
+              src="https://i.imgur.com/hNsZK7z.png"
+              alt="avatar"
+              style={{height: '200px', borderRadius: 100}}
+              />
+              <h2 style={{paddingTop: '2em'}}>Alex Beesley</h2>
+              <h4 style={{color: 'grey'}}>Programmer and web developer</h4>
+            </div>
+          </Cell>
+          <Cell className="resume-right-col" col={8}>Right</Cell>
+        </Grid>
       </div>
     )
   }
